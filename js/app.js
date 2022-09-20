806b630ccfa28a51dbe06a14eb2ac7b20f32257e
alexsharich <alexsharich@gmail.com>
2022-09-20 22:19:17 +0200

    add scripts for description Contacts,About,Skills

--- js/app.js
+++ js/app.js
@@ -6,3 +6,13 @@ ScrollSmoother.create({
   wrapper:'.wrapper',
   content:'.content'
 })
+
+const texts = document.querySelectorAll('.text')
+const buttons = document.querySelectorAll('.button')
+
+buttons.forEach(b=>{
+  b.addEventListener('click',(e)=>{
+   const data = e.target.getAttribute('data')
+    texts.forEach(el=>{(el.classList.contains(data)) ? el.classList.remove('hidden'): el.classList.add('hidden')  })
+  })
+}) 
